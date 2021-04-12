26efde89130487af6ff1f1dda84c9ce0807306c0
Jacob <jeck99@gmail.com>
2021-04-12 15:43:05 +0300

    added /

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === 'production') {
     // Serve any static files
     app.use(express.static(path.join(__dirname, '../client/build')));
     // Handle React routing, return all requests to React app
-    app.get('*', (req, res)=>{
+    app.get('/*', (req, res)=>{
         res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
     });
 }
