0c33b12de4f619a3f494571ea421642ea15bd47b
Jacob <jeck99@gmail.com>
2021-04-15 14:47:03 +0300

    a

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const app = express();
 const db = require('./DB');
 //import router module
 const moviesRouter = require('./routes/movie-router')
+const path = require('path');
 //configuration of the port based on the env
 const PORT = process.env.PORT || 8080;
 //use of body-parser in order to reach req.body
@@ -17,7 +18,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded())
 //use of cors so our server will be able to get requests
 app.use(cors())
-const path = require('path');
 db.on('error', () => { console.log("connection error") })
 app.listen(PORT, () => {
     console.log(`mern server is live and up on port: ${PORT}`);
@@ -26,10 +26,10 @@ app.use('/movies', moviesRouter)
 //*****************************************************************/
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    app.use(express.static(path.join(__dirname, "client", "build")))
+    app.use(express.static(path.join(__dirname, '../client/build')));
     // Handle React routing, return all requests to React app
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    app.get('*', (req, res)=>{
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
     });
-}
+  }
 //*******************************************************************/
